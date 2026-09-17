fb7614dc28118d8c28a8edea4fbf97ade6d7335d
TastyAndTheCats <tasty@tastyandthecats.com>
2026-09-17 07:13:40 -0400

    Added pyproject file and rewrote server.py to provide the app in a way that Vercel enjoys

--- server.py
+++ server.py
@@ -3,18 +3,22 @@ from flask import Flask, redirect
 # A simple server which accepts an http call (e.g. 0.0.0.0/rungameid/12345) and redirects to the steam://rungameid/12345 URL. This is needed because the Stream Deck software does not allow direct steam:// links.
 
 
-app = Flask("Steam Protocol Redirect")
+def create_app():
+    app = Flask("Steam Protocol Redirect")
+
+    @app.route("/")
+    def index():
+        return "Steam Protocol Redirect Server is running."
+
+    @app.route("/rungameid/<int:game_id>")
+    def run_game(game_id):
+        steam_url = f"steam://rungameid/{game_id}"
+        return redirect(steam_url)
+
+    return app()
 
 
-@app.route("/")
-def index():
-    return "Steam Protocol Redirect Server is running."
+app = create_app()
 
-
-@app.route("/rungameid/<int:game_id>")
-def run_game(game_id):
-    steam_url = f"steam://rungameid/{game_id}"
-    return redirect(steam_url)
-
-
-app.run()
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=5000)
