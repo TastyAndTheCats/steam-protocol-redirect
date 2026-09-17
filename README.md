b8e91a1e64353a2f8253544d161d0c71d7c178d9
TastyAndTheCats <tasty@tastyandthecats.com>
2026-09-17 04:04:40 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Steam Link Redirector
+
+This is a simple project that redirects rungameid/appid calls to the Steam client using the steam:// protocol.
+It is useful for launching games from a web browser or other applications (e.g. Stream Deck) that do not support the Steam protocol.
---
+++ server.py
@@ -0,0 +1,19 @@
+from flask import Flask, redirect
+
+# A simple server which accepts an http call (e.g. 0.0.0.0/rungameid/12345) and redirects to the steam://rungameid/12345 URL. This is needed because the Stream Deck software does not allow direct steam:// links.
+
+
+def run_game_server():
+
+    app = Flask(__name__)
+
+    @app.route("/rungameid/<int:game_id>")
+    def run_game(game_id):
+        steam_url = f"steam://rungameid/{game_id}"
+        return redirect(steam_url)
+
+    app.run(host="0.0.0.0", port=5000)
+
+
+if __name__ == "__main__":
+    run_game_server()
