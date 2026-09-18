ae09451dd8aab9e95528333385077048f9c062f8
TastyAndTheCats <tasty@tastyandthecats.com>
2026-09-18 19:42:36 -0400

    Changed it back

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template_string
+from flask import Flask, redirect
 
 # A simple server which accepts an http call (e.g. 0.0.0.0/rungameid/12345) and redirects to the steam://rungameid/12345 URL. This is needed because the Stream Deck software does not allow direct steam:// links.
 
@@ -13,28 +13,7 @@ def create_app():
     @flask_app.route("/rungameid/<int:game_id>")
     def run_game(game_id):
         steam_url = f"steam://rungameid/{game_id}"
-
-        # HTML with JavaScript to open new tab and close current one
-        html_content = f"""
-        <!DOCTYPE html>
-        <html>
-        <head>
-            <title>Redirecting...</title>
-            <script>
-                window.onload = function() {{
-                    // Open in a new tab
-                    window.open("{steam_url}", "_blank");
-                    // Close current tab
-                    window.close();
-                }};
-            </script>
-        </head>
-        <body>
-            <p>Redirecting to <a href="{steam_url}" target="_blank">{steam_url}</a>...</p>
-        </body>
-        </html>
-        """
-        return render_template_string(html_content)
+        return redirect(steam_url)
 
     return flask_app
 
